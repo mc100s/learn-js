db077ccabed74712f4941643fef3bcd059b4edac
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-26 15:53:28 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Route, Link, Switch } from 'react-router-dom' 
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+import Home from './pages/Home'
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar color="primary" dark expand="md">
+          <NavbarBrand href="/">Learn JS</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route render={props => <h1 className="text-center mt-4">404</h1>} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Home">
+        Home
+      </div>
+    );
+  }
+}
+
+export default App;
