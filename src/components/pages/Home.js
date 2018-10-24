d2fe450853b643ed6aee0fe42ef595b2651c4fb7
Maxence Bouret <maxence.bouret@gmail.com>
2018-10-24 23:56:55 +0200

    Improvement of landing page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import AceEditor from 'react-ace'
+import CodeSandbox from '../../components/CodeSandbox';
 import {
   Button
 } from 'reactstrap';
@@ -29,8 +29,107 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="Home container my-3 text-center" style={{ maxWidth: 700 }}>
-        <h1 className="">Learn Programming with JavaScript</h1>
+      <div className="Home text-center">
+        <div className="hero">
+          <div className="container">
+            <h1 className="">Learn Programming with JavaScript</h1>
+
+            {/* <ul>
+            <li>Small Courses</li>
+            <li>Interactive Exercises</li>
+            <li>Free</li>
+          </ul> */}
+
+            <div className="row properties align-items-center">
+              <div className="col-md-4">Small Courses</div>
+              <div className="col-md-4">Interactive Exercises</div>
+              <div className="col-md-4">Free</div>
+            </div>
+
+            <div className="my-5">
+              <Button color="primary" tag={Link} to="/course/intro-to-js" size="lg" >Go to the first course</Button>
+            </div>
+          </div>
+        </div>
+
+        <section className="container">
+          <h2>Why JavaScript?</h2>
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              JavaScript is currently the most popular languages for developers according to <a href="https://insights.stackoverflow.com/survey/2018/#most-popular-technologies">the Developer Survey 2018 from StackOverflow</a>.
+              <br /><br />
+              JavaScript gives you the tools to create awesome websites! When you master it, you can do front-end web development, back-end web development or even mobile development.
+              <br /><br />
+              Then when you master one programming language, all other are easier to get and let you understand things way faster.
+            </div>
+            <div className="col-md-6">
+              <img className="img-fluid" src="/images/most-popular-technologies.png" alt="" />
+            </div>
+          </div>
+        </section>
+
+        {/* <section className="container">
+
+          <CodeSandbox slug="types/numbers-1dsfsd" initialCodeContent={`
+var language = "fr";
+var greetings;
+if (language === "de") {
+  greetings = "Hallo";
+}
+else if (language === "fr") {
+  greetings = "Bonjour";
+}
+else {
+  greetings = "Hello";
+}
+`} testContent={`
+// Tests
+testEquality(greetings, "Bonjour");
+`} />
+        </section> */}
+
+
+        <section className="container">
+          <h2>The methodology</h2>
+          <div className="row">
+            <div className="col-md-4">
+              <h3>Theory</h3>
+              <img className="img-fluid" src="/images/screenshot-course.png" alt="screenshot-course" />
+            </div>
+            <div className="col-md-4">
+              <h3>Exercises</h3>
+              <img className="img-fluid" src="/images/screenshot-input.png" alt="screenshot-input" />
+            </div>
+            <div className="col-md-4">
+              <h3>Feedback</h3>
+              <img className="img-fluid" src="/images/screenshot-output.png" alt="screenshot-output" />
+            </div>
+          </div>
+        </section>
+
+        <section className="container">
+          <h2>Your instructor</h2>
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <img className="img-fluid img-instructor" src="/images/maxence.jpeg" alt="maxence" />
+            </div>
+            <div className="col-md-6">
+              Maxence dived into code and web development since he was 14. He has a diverse experience in the tech ecosystem where we worked for many companies as a freelancer and launched a startup project.
+              <br /><br />
+              He is now a Lead Instructor at Ironhack where is obsession is to train beginners to become web developers.
+              <br /><br />
+              We wrote the course and the exercises so you can learn good practices in a playful environment.
+            </div>
+          </div>
+        </section>
+
+        <section className="container">
+          <h2>Are you ready?</h2>
+          <div className="my-5">
+            <Button color="primary" tag={Link} to="/course/intro-to-js" size="lg" >Go to the first course</Button>
+          </div>
+        </section>
+
 
         {/* 
         Change typo
@@ -38,17 +137,11 @@ class Home extends Component {
         The content learned during the course
         */}
 
-        <p className="my-3">Welcome to this platform where you will learn the basics of programming with JavaScript, through courses and many online exercises.</p>
-        <p className="my-3">Why JavaScript? Because it's one of the most popular languages for web developers that can give you the tools to create awesome websites!!</p>
 
-        <div className="my-5">
-          <Button color="primary" tag={Link} to="/course/intro-to-js" size="lg" >Go to the first course</Button>
-        </div>
-
-        <h2 className="mt-5">Developed by</h2>
+        {/* <h2 className="mt-5">Developed by</h2>
         <div className="my-3">
-          <img src="/ironhack-logo.png" alt="" style={{ maxWidth: "50%", width: 150 }} />
-        </div>
+          <img src="/images/ironhack-logo.png" alt="" style={{ maxWidth: "50%", width: 150 }} />
+        </div> */}
       </div>
     );
   }
